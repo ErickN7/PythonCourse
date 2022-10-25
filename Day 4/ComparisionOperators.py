4081958a323c897ea5c44324edca7c6ecf584030
Kubo Developer 7 <kubodeveloper7@outlook.com>
2022-10-25 18:05:09 -0500

    Part of Day 4

---
+++ Day 4/ComparisionOperators.py
@@ -0,0 +1,6 @@
+my_bool = 10 == 25
+print(my_bool)
+my_bool2 = 'blanco' == 'Blanco'.lower()
+print(my_bool2)
+my_bool3 = 100.0 == 100
+print(my_bool3)
---
+++ Day 4/LogicalOperators.py
@@ -0,0 +1,7 @@
+my_bool = (4 < 5) and (5 > 6)
+my_bool = (4 < 5) or (5 > 6)
+print(my_bool)
+text = "esta frase es breve"
+my_bool = ('frase' in text) or ('python' in text)
+my_bool = not ('a' == 'a')
+print(my_bool)
